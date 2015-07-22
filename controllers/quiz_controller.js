a91a324a1000bc9941f99294f0ddfd03de3e86a6
ximito <ximo.pozo@gmail.com>
2015-07-22 11:58:22 +0200

    Se actualiza quiz con las preguntas almacenadas en BD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -75,7 +75,7 @@ var models = require('../models/models.js');
 
 //Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function (req,res,next,quizId){
-  models.Quiz.find(quizId).then(
+  models.Quiz.find(req.params.quizId).then(
       function(quiz){
         if(quiz){
           req.quiz = quiz;
